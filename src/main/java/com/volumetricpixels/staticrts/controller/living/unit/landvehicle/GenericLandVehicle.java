faf414a96627500ce0d510c1a3457ce43a39403a
IronCrystal <apeace95@gmail.com>
2012-06-26 10:40:11 -0400

    Added more Units

---
+++ src/main/java/com/volumetricpixels/staticrts/controller/living/unit/landvehicle/GenericLandVehicle.java
@@ -0,0 +1,11 @@
+package com.volumetricpixels.staticrts.controller.living.unit.landvehicle;
+
+import com.volumetricpixels.staticrts.controller.StaticControllerType;
+import com.volumetricpixels.staticrts.controller.living.unit.Unit;
+
+public abstract class GenericLandVehicle extends Unit {
+
+	public GenericLandVehicle(StaticControllerType type) {
+		super(type);
+	}
+}
